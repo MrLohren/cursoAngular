dc2e4530abe20b07fd61ddc32ed9b566759cb278
Felipe Lagos <nachitofilm@gmail.com>
2024-07-15 11:48:42 -0400

    forms dinamicos

--- 07-formsApp/src/app/reactive/pages/basic-pace/basic-pace.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-pace/basic-pace.component.ts
@@ -8,12 +8,6 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 })
 export class BasicPaceComponent implements OnInit {
 
-  // public myForm : FormGroup = new FormGroup({
-  //   name      : new FormControl(''),
-  //   price     : new FormControl(0),
-  //   inStorage : new FormControl(0),
-  // })
-
   public myForm : FormGroup = this.fb.group({
     name      : ['', [ Validators.required, Validators.minLength(3) ]],
     price     : [0, [Validators.required, Validators.min(0)]],
@@ -30,9 +24,39 @@ export class BasicPaceComponent implements OnInit {
     })
   }
 
+  isValidField ( field : string ) : boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field : string ) : string | null {
+
+    if( !this.myForm.controls[field] ) return null
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for(const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este campo es Requerido.'
+
+        case 'minlength':
+          return `Este campo requiere minimo ${errors['minlength'].requiredLength} letras`
+      }
+    }
+
+    return null
+
+  }
+
   onSave() : void {
 
-    if(this.myForm.invalid) return
+    if(this.myForm.invalid) {
+
+      this.myForm.markAllAsTouched()
+
+      return
+    }
 
     console.log(this.myForm.value)
 
--- 07-formsApp/src/app/reactive/pages/dynamic-pace/dynamic-pace.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-pace/dynamic-pace.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-pace',
@@ -7,4 +8,75 @@ import { Component } from '@angular/core';
 })
 export class DynamicPaceComponent {
 
+  public myForm : FormGroup = this.fb.group({
+    name : ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames : this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  })
+
+  public newFavorite : FormControl = new FormControl('', Validators.required)
+
+  constructor(private fb : FormBuilder) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray
+  }
+
+  isValidFieldInArray( formArray : FormArray, index : number ) {
+    return formArray.controls[index].errors
+    && formArray.controls[index].touched
+  }
+
+  isValidField ( field : string ) : boolean | null {
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field : string ) : string | null {
+
+    if( !this.myForm.controls[field] ) return null
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for(const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'Este campo es Requerido.'
+
+        case 'minlength':
+          return `Este campo requiere minimo ${errors['minlength'].requiredLength} letras`
+      }
+    }
+
+    return null
+
+  }
+
+  onAddToFavorites() : void {
+    if( this.newFavorite.invalid ) return
+
+    const newGame = this.newFavorite.value
+
+    this.favoriteGames.push(
+      this.fb.control(newGame, Validators.required)
+    )
+
+    this.newFavorite.reset()
+
+  }
+
+  onDeleteFavorite(index : number) : void {
+    this.favoriteGames.removeAt(index)
+  }
+
+  onSubmit() : void {
+    if( !this.myForm.valid ) {
+      this.myForm.markAllAsTouched()
+      return
+    }
+    (this.myForm.controls['favoriteGames'] as FormArray ) = this.fb.array([])
+    this.myForm.reset()
+  }
 }
